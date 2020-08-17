521d495b61e71af1b5398efa8037896149cabcf1
Dion Olympia <dionolympia@gmail.com>
2020-08-17 00:34:36 -0400

    finished routing for RESTful API (create, get all, get by id, update, delete)

--- server/index.js
+++ server/index.js
@@ -4,36 +4,87 @@ const cors = require("cors");
 const pool = require("./db");
 
 // middleware
-app.use(cors());
+app.use(cors()); // allows for cross
 app.use(express.json()); // lets us use req.body
 
-// routes
+/* start of routes */
 
 // create a todo
 app.post("/todos", async (req, res) => {
     try {
         const { description } = req.body;
         const newTodo = await pool.query(
-            "INSERT INTO todo (description) VALUES($1)",
+            "INSERT INTO todo (description) VALUES($1) RETURNING *",
             [description]
         );
-
-        res.json(newTodo);
-    } catch (error) {
-        console.error(error.message);
+        res.json(newTodo.rows[0]);
+    } catch (err) {
+        console.error(err.message);
     }
-
-    return "todos api route works";
 });
 
 // get all todos
+app.get("/todos", async (req, res) => {
+    try {
+        const allTodos = await pool.query("SELECT * FROM todo");
+        res.json(allTodos.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
 
-// get a todo
+// get a specific todo
+app.get("/todos/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const specTodo = await pool.query(
+            "SELECT * FROM todo WHERE todo_id = $1",
+            [id]
+        );
+        res.json(specTodo.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
 
 // update a todo
+app.put("/todos/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { description } = req.body;
+        const updateTodo = await pool.query(
+            "UPDATE todo SET description = $1 WHERE todo_id = $2",
+            [description, id]
+        );
+        res.json(
+            "Todo with id:" +
+                id +
+                " was updated with description:'" +
+                description +
+                "'"
+        );
+    } catch (err) {
+        console.error(err.message);
+    }
+});
 
 // delete a todo
+app.delete("/todos/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deleteTodo = await pool.query(
+            "DELETE FROM todo WHERE todo_id = $1",
+            [id]
+        );
+        res.json("Todo with id:" + id + " was deleted");
+    } catch (err) {
+        console.error(err.message);
+    }
+});
 
+/* end of routes */
+
+// Listening on port 5000
 app.listen(5000, () => {
     console.log("server has started on port 5000");
 });
